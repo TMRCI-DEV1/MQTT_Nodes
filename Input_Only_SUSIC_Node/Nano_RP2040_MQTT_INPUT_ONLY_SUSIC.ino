7c859143e08830cb7df761214f3f419ba0cd54e9
The Model Railroad Club, Inc <thomas.seitz@tmrci.org>
2023-05-26 02:08:50 -0400

    Update Nano_RP2040_MQTT_INPUT_ONLY_SUSIC.ino
    Updated MQTT topic handling and enabled message retention for published messages.

--- Input_Only_SUSIC_Node/Nano_RP2040_MQTT_INPUT_ONLY_SUSIC.ino
+++ Input_Only_SUSIC_Node/Nano_RP2040_MQTT_INPUT_ONLY_SUSIC.ino
@@ -1,8 +1,8 @@
 /*
   Project: Arduino-Nano RP2040 based WiFi CMRI/MQTT enabled SUSIC Input-ONLY Node (72 inputs)
   Author: Thomas Seitz (thomas.seitz@tmrci.org)
-  Version: 1.0.5
-  Date: 2023-05-24
+  Version: 1.0.6
+  Date: 2023-05-26
   Description: A sketch for an Arduino-Nano RP2040 based CMRI SUSIC Input-ONLY Node (72 inputs) 
   using MQTT to publish messages subscribed to by JMRI. Message payload is either ACTIVE or INACTIVE.
 */
@@ -16,6 +16,9 @@
 const char ssid[] = "HO Touch Panels";     // Name of the WiFi network
 const char password[] = "touch.666.pi";    // Password of the WiFi network
 const char *mqtt_server = "192.168.50.50"; // IP address of the MQTT server
+
+// MQTT topic constants
+const char* MQTT_TOPIC_PREFIX_SENSOR = "TMRCI/input/";
 
 // Define the latch pin for 74HC165 Shift Register
 const byte LATCH_165 = 9;
@@ -31,8 +34,8 @@ byte last_input_state[9]; // 72 inputs from 9 74HC165s
 const char* NodeID = "10-A-Node-1"; // ***CHANGE TO APPROPRIATE UNIQUE ID***
 
 // Range of the sensor IDs
-const int minSensorId = 1;    // Change to 1
-const int maxSensorId = 72;   // Change to 72
+const int minSensorId = 1;
+const int maxSensorId = 72;
 
 void setup() {
   // Initialize the latch pin for the shift registers as output
@@ -89,9 +92,9 @@ void loop() {
     int byteIndex = arrayIndex / 8;
     int bitIndex = arrayIndex % 8;
     if (bitRead(currentInputState[byteIndex], bitIndex) != bitRead(last_input_state[byteIndex], bitIndex)) {
-      String topic = String("TMRCI/dt/sensor/") + String(NodeID) + "/S" + String(i);
+      String topic = String(MQTT_TOPIC_PREFIX_SENSOR) + String(NodeID) + "/sensor/S" + String(i);
       String payload = (bitRead(currentInputState[byteIndex], bitIndex) == 1) ? "ACTIVE" : "INACTIVE";
-      client.publish(topic.c_str(), payload.c_str());
+      client.publish(topic.c_str(), payload.c_str(), true);
     }
   }
   
