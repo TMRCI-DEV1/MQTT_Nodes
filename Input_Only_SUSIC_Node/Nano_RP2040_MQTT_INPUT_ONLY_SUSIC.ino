615c36745258e2aebe492226564766fe373d9524
The Model Railroad Club, Inc <thomas.seitz@tmrci.org>
2023-05-26 23:29:25 -0400

    Update Nano_RP2040_MQTT_INPUT_ONLY_SUSIC.ino
    Cleaned up code and added proper commenting.

--- Input_Only_SUSIC_Node/Nano_RP2040_MQTT_INPUT_ONLY_SUSIC.ino
+++ Input_Only_SUSIC_Node/Nano_RP2040_MQTT_INPUT_ONLY_SUSIC.ino
@@ -1,9 +1,9 @@
 /*
   Project: Arduino-Nano RP2040 based WiFi CMRI/MQTT enabled SUSIC Input-ONLY Node (72 inputs)
   Author: Thomas Seitz (thomas.seitz@tmrci.org)
-  Version: 1.0.8
+  Version: 1.1.0
   Date: 2023-05-26
-  Description: A sketch for an Arduino-Nano RP2040 based CMRI SUSIC Input-ONLY Node (72 inputs) 
+  Description: A sketch for an Arduino-Nano RP2040 based CMRI SUSIC Input-ONLY Node (72 inputs)
   using MQTT to publish messages subscribed to by JMRI. Message payload is either ACTIVE or INACTIVE.
 */
 
@@ -15,12 +15,12 @@
 // Network configuration
 const char ssid[] = "HO Touch Panels";     // Name of the WiFi network
 const char password[] = "touch.666.pi";    // Password of the WiFi network
-const char *mqtt_server = "192.168.50.50"; // IP address of the MQTT server
+const char* mqtt_server = "192.168.50.50"; // IP address of the MQTT server
 
 // MQTT topic constants
 const char* MQTT_TOPIC_PREFIX_SENSOR = "TMRCI/input/";
 
-// Define the latch pin for 74HC165 Shift Register
+// Define pins for 74HC165 (input shift register)
 const byte LATCH_165 = 9;
 
 // Instantiate MQTT client
@@ -30,8 +30,8 @@ PubSubClient client(espClient);
 // Array to store the last state of the inputs
 byte last_input_state[9]; // 72 inputs from 9 74HC165s
 
-// Identifier of the Arduino
-const char* NodeID = "10-A-Node-1"; // ***CHANGE TO APPROPRIATE UNIQUE ID***
+// Identifier of the Node
+const char* NodeID = "10-A-Node-2"; // ***CHANGE TO APPROPRIATE UNIQUE ID***
 
 // Range of the sensor IDs
 const int minSensorId = 1;
@@ -50,7 +50,7 @@ void setup() {
   Serial.print("Connecting to ");
   Serial.println(ssid);
   WiFi.begin(ssid, password);
-  
+
   // Check if the WiFi connection is established
   while (WiFi.status() != WL_CONNECTED) {
     delay(500);
@@ -78,12 +78,12 @@ void loop() {
   digitalWrite(LATCH_165, LOW);
   delayMicroseconds(5);
   digitalWrite(LATCH_165, HIGH);
-  
+
   byte currentInputState[9]; // Store current state of inputs
-  
+
   // Iterate over each shift register to read its current state
   for (int i = 0; i < 9; i++) {
-    currentInputState[i] = SPI.transfer(0);
+    currentInputState[i] = ~SPI.transfer(0); // Invert the bits to reflect active low inputs
   }
 
   // Publish input state changes over MQTT
@@ -97,32 +97,29 @@ void loop() {
       client.publish(topic.c_str(), payload.c_str(), true);
     }
   }
-  
-  // Copy the current input state to the last input state for the next comparison
+
+  // Update the last input state for the next comparison
   memcpy(last_input_state, currentInputState, 9);
-  
-  // Add a delay before next loop
+
+  // Delay before next loop iteration
   delay(10);
 }
 
+void callback(char* topic, byte* payload, unsigned int length) {
+  // Handle incoming MQTT messages if required
+}
+
 void reconnect() {
-  // Loop until reconnected to the MQTT server
   while (!client.connected()) {
     Serial.print("Attempting MQTT connection...");
-    
-    // Attempt to connect to the MQTT server
     if (client.connect(NodeID)) {
       Serial.println("connected");
+      // Subscribe to MQTT topics if required
     } else {
       Serial.print("failed, rc=");
       Serial.print(client.state());
       Serial.println(" try again in 5 seconds");
-      // Wait 5 seconds before retrying connection
       delay(5000);
     }
   }
-}
-
-void callback(char* topic, byte* payload, unsigned int length) {
-  // This function is empty because the device is input only and doesn't control any outputs
 }
