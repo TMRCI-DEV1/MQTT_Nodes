13f494dfa3e464759b143daa6416682f21646ac1
The Model Railroad Club, Inc <thomas.seitz@tmrci.org>
2023-06-13 01:58:23 -0400

    Update OLED_2_SL2abs_4_SL1low_1_SL2low_NEO.ino
    Updated OLED display code to only update display upon change of NodeID or IP address.

--- ESP32/NeoPixel_Signal_Controllers/OLED_2_SL2abs_4_SL1low_1_SL2low_NEO.ino
+++ ESP32/NeoPixel_Signal_Controllers/OLED_2_SL2abs_4_SL1low_1_SL2low_NEO.ino
@@ -2,8 +2,8 @@
   Project: ESP32 based WiFi/MQTT enabled (2) Double Searchlight High Absolute, (4) Single Head Dwarf, and (1) Double Head Dwarf signal Neopixel Node
   (7 signal mast outputs / 10 Neopixel Signal Heads)
   Author: Thomas Seitz (thomas.seitz@tmrci.org)
-  Version: 1.0.3
-  Date: 2023-06-12
+  Version: 1.0.4
+  Date: 2023-06-13
   Description: This sketch is designed for an ESP32 Node with 7 signal mast outputs, using MQTT to subscribe to messages published by JMRI.
   The expected incoming subscribed messages are for JMRI Signal Mast objects, and the expected message payload format is 'Aspect; Lit (or Unlit); Unheld (or Held)'.
   NodeID and IP address displayed on attached 128×64 OLED display.
@@ -286,6 +286,9 @@ void callback(char* topic, byte* payload, unsigned int length) {
 
 void updateDisplay() {
     // Check if NodeID or IP address changed
+    static String previousNodeID = "";
+    static String previousIPAddress = "";
+
     if (NodeID != previousNodeID || WiFi.localIP().toString() != previousIPAddress) {
         // Update NodeID and IP address
         previousNodeID = NodeID;
@@ -295,13 +298,13 @@ void updateDisplay() {
         display.clearDisplay();
 
         // Display NodeID with larger text
-        display.setTextSize(2); // Increase text size
+        display.setTextSize(2);
         display.setTextColor(WHITE);
-        display.setCursor(0, 0); // Start at top-left corner
+        display.setCursor(0, 0);
         display.println("NodeID: " + NodeID);
 
         // Display IP address with smaller text
-        display.setTextSize(1); // Decrease text size for IP address
+        display.setTextSize(1);
         display.println("IP address: " + WiFi.localIP().toString());
 
         display.display();
