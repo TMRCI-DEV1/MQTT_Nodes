ac5c42ab273e17b972ebecc6e3bc43918635a289
The Model Railroad Club, Inc <thomas.seitz@tmrci.org>
2023-06-13 02:23:06 -0400

    Update OLED_8_SL2abs_NEO.ino

--- ESP32/NeoPixel_Signal_Controllers/OLED_8_SL2abs_NEO.ino
+++ ESP32/NeoPixel_Signal_Controllers/OLED_8_SL2abs_NEO.ino
@@ -35,7 +35,7 @@ const int SCREEN_WIDTH = 128; // OLED display width, in pixels
 const int SCREEN_HEIGHT = 64; // OLED display height, in pixels
 
 // Declaration for an SSD1306 display connected to I2C (SDA, SCL pins)
-const int OLED_RESET = -1; 
+const int OLED_RESET = -1; // Reset pin # (or -1 if sharing ESP32 reset pin)
 Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
 
 // Define the GPIO pins for the Neopixels in ascending order
