83ac3dd6834662c7a9d0fb6045697aa7e3166950
The Model Railroad Club, Inc <thomas.seitz@tmrci.org>
2023-06-14 00:36:38 -0400

    Update OLED_2_SL2abs_3_SL1low_NEO.ino
    Updated commenting.

--- ESP32/NeoPixel_Signal_Controllers/OLED_2_SL2abs_3_SL1low_NEO.ino
+++ ESP32/NeoPixel_Signal_Controllers/OLED_2_SL2abs_3_SL1low_NEO.ino
@@ -2,8 +2,8 @@
   Project: ESP32 based WiFi/MQTT enabled (2) Double Searchlight High Absolute and (3) Single Head Dwarf signal Neopixel Node
   (5 signal mast outputs / 7 Neopixel Signal Heads)
   Author: Thomas Seitz (thomas.seitz@tmrci.org)
-  Version: 1.0.2
-  Date: 2023-06-13
+  Version: 1.0.3
+  Date: 2023-06-14
   Description: This sketch is designed for an ESP32 Node with 5 signal mast outputs, using MQTT to subscribe to messages published by JMRI.
   The expected incoming subscribed messages are for JMRI Signal Mast objects, and the expected message payload format is 'Aspect; Lit (or Unlit); Unheld (or Held)'.
   NodeID and IP address displayed on attached 128×64 OLED display.
@@ -43,11 +43,11 @@ const int neoPixelPins[5] = {16, 17, 18, 19, 23};
 
 // Define the Neopixel chains and signal masts
 Adafruit_NeoPixel signalMasts[5] = {                             // Array of Neopixels, one for each signal mast
-    Adafruit_NeoPixel(2, neoPixelPins[0], NEO_GRB + NEO_KHZ800), // SM1
-    Adafruit_NeoPixel(2, neoPixelPins[1], NEO_GRB + NEO_KHZ800), // SM2
-    Adafruit_NeoPixel(1, neoPixelPins[2], NEO_GRB + NEO_KHZ800), // SM3
-    Adafruit_NeoPixel(1, neoPixelPins[3], NEO_GRB + NEO_KHZ800), // SM4
-    Adafruit_NeoPixel(1, neoPixelPins[4], NEO_GRB + NEO_KHZ800), // SM5
+    Adafruit_NeoPixel(2, neoPixelPins[0], NEO_GRB + NEO_KHZ800), // SM1 (double head absolute)
+    Adafruit_NeoPixel(2, neoPixelPins[1], NEO_GRB + NEO_KHZ800), // SM2 (double head absolute)
+    Adafruit_NeoPixel(1, neoPixelPins[2], NEO_GRB + NEO_KHZ800), // SM3 (single head dwarf)
+    Adafruit_NeoPixel(1, neoPixelPins[3], NEO_GRB + NEO_KHZ800), // SM4 (single head dwarf)
+    Adafruit_NeoPixel(1, neoPixelPins[4], NEO_GRB + NEO_KHZ800), // SM5 (single head dwarf)
 };
 
 // Define the NodeID and MQTT topic
