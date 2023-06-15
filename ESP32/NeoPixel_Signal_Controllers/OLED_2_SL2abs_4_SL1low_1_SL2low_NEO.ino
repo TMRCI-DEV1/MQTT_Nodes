d399a3bae01a76ec61d217b59dbbe2fe8fa39306
The Model Railroad Club, Inc <thomas.seitz@tmrci.org>
2023-06-14 23:58:06 -0400

    Update OLED_2_SL2abs_4_SL1low_1_SL2low_NEO.ino
    Updated commenting and test NodeID.

--- ESP32/NeoPixel_Signal_Controllers/OLED_2_SL2abs_4_SL1low_1_SL2low_NEO.ino
+++ ESP32/NeoPixel_Signal_Controllers/OLED_2_SL2abs_4_SL1low_1_SL2low_NEO.ino
@@ -2,8 +2,8 @@
   Project: ESP32 based WiFi/MQTT enabled (2) Double Searchlight High Absolute, (4) Single Head Dwarf, and (1) Double Head Dwarf signal Neopixel Node
   (7 signal mast outputs / 10 Neopixel Signal Heads)
   Author: Thomas Seitz (thomas.seitz@tmrci.org)
-  Version: 1.0.6
-  Date: 2023-06-14
+  Version: 1.0.7
+  Date: 2023-06-15
   Description: This sketch is designed for an ESP32 Node with 7 signal mast outputs, using MQTT to subscribe to messages published by JMRI.
   The expected incoming subscribed messages are for JMRI Signal Mast objects, and the expected message payload format is 'Aspect; Lit (or Unlit); Unheld (or Held)'.
   NodeID and IP address displayed on attached 128×64 OLED display.
@@ -53,7 +53,7 @@ Adafruit_NeoPixel signalMasts[7] = {                             // Array of Neo
 };
 
 // Define the NodeID and MQTT topic
-String NodeID = "10-A-Node-1";                                // Node identifier
+String NodeID = "10-A-Node-2";                                // Node identifier
 String mqttTopic = "TMRCI/output/" + NodeID + "/signalmast/"; // Base MQTT topic
 
 // Variables to track NodeID and IP address
@@ -177,7 +177,7 @@ void reconnectMQTT() {
     while (!client.connected()) {
         Serial.println("Attempting to connect to MQTT...");
         if (client.connect(NodeID.c_str())) {
-            client.subscribe((mqttTopic + "#").c_str());      // Subscribe to topics for all signal masts
+            client.subscribe((mqttTopic + "+").c_str());      // Subscribe to topics for all signal masts
             Serial.println("Connected to MQTT");
         } else {
             delay(5000);
